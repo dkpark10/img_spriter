3d9d7e6bcff6e4902705864445ee804205e06d8a
dkpark10 <dkpark10@naver.com>
2022-04-09 22:46:53 +0900

    [add]publicPath 수정 및 파일로더 설치

--- src/app.tsx
+++ src/app.tsx
@@ -2,37 +2,23 @@ import React, { useState, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { increment, decrement } from './reducer/counter';
 import { RootState } from './reducer/index';
+import image from './assets/img0.jpg';
 
 export default function App(): JSX.Element {
 
   const dispatch = useDispatch();
   const data = useSelector((state: RootState) => state.count.data);
+  console.log(image);
 
   return (
     <>
       <h2>{data}</h2>
       <button onClick={() => dispatch(increment())}>increase</button>
       <button onClick={() => dispatch(decrement())}>decrease</button>
+      <main>
+        <img src={image}/>
+        <label>{process.env.NODE_ENV}</label>
+      </main>
     </>
   )
 }
-
-// export default function App(): JSX.Element {
-
-//   const [data, setData] = useState<number>(0);
-//   const inc = () => {
-//     setData(data + 1);
-//   }
-
-//   const dec = () => {
-//     setData(data - 1);
-//   }
-
-//   return (
-//     <>
-//       <h2>{data}</h2>
-//       <button onClick={inc}>increase</button>
-//       <button onClick={dec}>decrease</button>
-//     </>
-//   )
-// }
