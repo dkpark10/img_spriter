7e3b080d381491ba46a9bef08d6cc2c4b8cda0fb
dkpark10 <dkpark10@naver.com>
2023-01-01 03:05:40 +0900

    [feat]: 사이즈 조절 컴포넌트 마우스 무브 기능 추가

--- src/components/size_dot.tsx
+++ src/components/size_dot.tsx
@@ -1,4 +1,9 @@
-import React, { useState, useRef, RefObject } from 'react';
+import React, {
+  useState,
+  useRef,
+  RefObject,
+  useEffect,
+} from 'react';
 import { Size, Coord } from 'custom-type';
 
 interface Props {
@@ -9,68 +14,128 @@ interface Props {
 export default function SizeDot({ size, target }: Props) {
   const [isMouseDown, setIsMouseDown] = useState(false);
 
-  const [initCoord, setInitCoord] = useState<Coord>({ y: 0, x: 0 });
+  const [currentDotCoord, setCurrentDotCoord] = useState<Coord[]>([]);
 
-  const onMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+  useEffect(() => {
+    setCurrentDotCoord([
+      { y: 0, x: 0 },
+      { y: size.height, x: 0 },
+      { y: 0, x: size.width },
+      { y: size.height, x: size.width },
+    ]);
+  }, [size.width, size.height]);
+
+  const dotRef = useRef<HTMLButtonElement[]>([]);
+
+  const setDotButtonElement = (element: HTMLButtonElement | null, idx: number) => {
+    if (element === null) {
+      return;
+    }
+
+    dotRef.current[idx] = element;
+  };
+
+  const onMouseDown = () => {
     if (!target.current) {
       return;
     }
 
     setIsMouseDown(true);
-
-    const { offsetTop, offsetLeft } = target.current;
-    const y = e.pageY - offsetTop;
-    const x = e.pageX - offsetLeft;
-
-    setInitCoord((prev) => ({
-      ...prev,
-      y,
-      x,
-    }));
   };
 
-  const onMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!isMouseDown || !target.current) {
+  const onMouseMove = (e: React.MouseEvent<HTMLButtonElement>, idx: number) => {
+    if (isMouseDown === false) {
       return;
     }
 
-    const { offsetLeft, offsetTop } = target.current;
+    setCurrentDotCoord((prev) => {
+      const { movementX, movementY } = e;
+      const newCurrentCoord = [...prev];
+      newCurrentCoord[idx].x += movementX;
+      newCurrentCoord[idx].y += movementY;
+
+      return newCurrentCoord;
+    });
+  };
+
+  const onMouseUp = () => {
+    setIsMouseDown(false);
   };
 
   return (
     <>
-      <div
-        className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400 
-        cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
-        style={{ top: 0, left: 0 }}
-        role='button'
-        onMouseDown={onMouseDown}
-        tabIndex={0}
-      />
-      <div
-        className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400 
-        cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
-        style={{ top: size.height, left: 0 }}
-        role='button'
-        onMouseDown={onMouseDown}
-        tabIndex={0}
-      />
-      <div
-        className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400 
-        cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
-        style={{ top: 0, left: size.width }}
-        role='button'
-        onMouseDown={onMouseDown}
-        tabIndex={0}
-      />
-      <div
-        className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400 
-        cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
-        style={{ top: size.height, left: size.width }}
-        role='button'
-        onMouseDown={onMouseDown}
-        tabIndex={0}
-      />
+      {currentDotCoord.map((item, idx) => (
+        <button
+          // eslint-disable-next-line react/no-array-index-key
+          key={idx}
+          className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400 
+              cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
+          style={{ top: item.y, left: item.x }}
+          onMouseDown={onMouseDown}
+          onMouseMove={(e) => onMouseMove(e, idx)}
+          onMouseUp={onMouseUp}
+          aria-label='size-controller'
+          type='button'
+          ref={(el) => setDotButtonElement(el, idx)}
+        />
+      ))}
     </>
   );
+
+  // return (
+  //   <>
+  //     <button
+  //       className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400
+  //       cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
+  //       style={{ top: currentDotCoord[0].y, left: currentDotCoord[0].x }}
+  //       onMouseDown={(e) => onMouseDown(e, 0)}
+  //       onMouseMove={onMouseMove}
+  //       onMouseUp={onMouseUp}
+  //       aria-label='size-controller1'
+  //       type='button'
+  //       ref={(el) => (
+  //         setDotButtonElement(el, 0)
+  //       )}
+  //     />
+  //     <button
+  //       className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400
+  //       cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
+  //       style={{ top: currentDotCoord[1].y, left: currentDotCoord[1].x }}
+  //       onMouseDown={(e) => onMouseDown(e, 1)}
+  //       onMouseMove={onMouseMove}
+  //       onMouseUp={onMouseUp}
+  //       aria-label='size-controller2'
+  //       type='button'
+  //       ref={(el) => (
+  //         setDotButtonElement(el, 1)
+  //       )}
+  //     />
+  //     <button
+  //       className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400
+  //       cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
+  //       style={{ top: currentDotCoord[2].y, left: currentDotCoord[2].x }}
+  //       onMouseDown={(e) => onMouseDown(e, 2)}
+  //       onMouseMove={onMouseMove}
+  //       onMouseUp={onMouseUp}
+  //       aria-label='size-controller3'
+  //       type='button'
+  //       ref={(el) => (
+  //         setDotButtonElement(el, 2)
+  //       )}
+  //     />
+  //     <button
+  //       className={`absolute w-[12px] h-[12px] rounded-full bg-teal-400
+  //       cursor-pointer translate-x-[-50%] translate-y-[-50%]`}
+  //       style={{ top: currentDotCoord[3].y, left: currentDotCoord[3].x }}
+  //       onMouseDown={(e) => onMouseDown(e, 3)}
+  //       onMouseMove={onMouseMove}
+  //       onMouseUp={onMouseUp}
+  //       aria-label='size-controller4'
+  //       type='button'
+  //       ref={(el) => (
+  //         setDotButtonElement(el, 3)
+  //       )}
+  //     />
+  //   </>
+  // );
 }
