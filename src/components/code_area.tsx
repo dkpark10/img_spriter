97246ea46a23b8eb6b6f4563a0b9da31eb6b4136
dkpark10 <dkpark10@naver.com>
2023-01-03 01:20:49 +0900

    [fix]: 코드 에어리어 css 속성 컨텐츠 수정

--- src/components/code_area.tsx
+++ src/components/code_area.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import { useRecoilValue } from 'recoil';
-import { spriteSizeState, imageSrcState } from '../store/index';
+import {
+  spriteSizeState,
+  imageSrcState,
+  imageScaleState,
+  imageSizeState,
+} from '../store/index';
 
 export default function CodeArea() {
   const {
@@ -9,6 +15,10 @@ export default function CodeArea() {
 
   const imgSrc = useRecoilValue(imageSrcState);
 
+  const imageScale = useRecoilValue(imageScaleState);
+
+  const imageSize = useRecoilValue(imageSizeState);
+
   return (
     <div className='flex items-center justify-center m-3'>
       <div className='bg-[#292c39] text-[#cacaca] rounded-md p-4'>
@@ -16,20 +26,31 @@ export default function CodeArea() {
         <div>
           &nbsp;&nbsp;&nbsp;background:&nbsp;
           <span className='text-[#44e149]'>url</span>
-          {`('${imgSrc}')`}
-          {` no-repeat -${y}px; -${x}px;`}
+          {`('${imgSrc}') no-repeat;`}
+        </div>
+        <div>
+          &nbsp;&nbsp;&nbsp;background-position:&nbsp;
+          <span className='text-[#f08a15]'>
+            {`${-x}px ${-y}px`}
+          </span>;
+        </div>
+        <div>
+          &nbsp;&nbsp;&nbsp;background-size:&nbsp;
+          <span className='text-[#f08a15]'>
+            {`${imageSize.width * imageScale}px ${imageSize.height * imageScale}px`}
+          </span>;
         </div>
         <div>
           &nbsp;&nbsp;&nbsp;width:&nbsp;
           <span className='text-[#f08a15]'>
             {`${width}px`}
-          </span>
+          </span>;
         </div>
         <div>
           &nbsp;&nbsp;&nbsp;height:&nbsp;
           <span className='text-[#f08a15]'>
             {`${height}px`}
-          </span>
+          </span>;
         </div>
         <div>{'}'}</div>
       </div>
