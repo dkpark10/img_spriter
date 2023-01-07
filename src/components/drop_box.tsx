9ab2be3ced70f64b62ae0c45057c0a0b8c4be5c9
dkpark10 <dkpark10@naver.com>
2023-01-08 00:58:13 +0900

    [feat]: 인풋 파일로 이미지 올리도록 기능 추가

--- src/components/drop_box.tsx
+++ src/components/drop_box.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { imageSrcState } from '../store';
 
 export default function DropDropBox() {
+  const setImageSrc = useSetRecoilState(imageSrcState);
+
+  const inputFileRef = useRef<HTMLInputElement>(null);
+
+  const onClick = () => {
+    inputFileRef.current?.click();
+  };
+
+  const onChagne = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (event: ProgressEvent<FileReader>) => {
+      if (typeof event.target?.result !== 'string') {
+        return;
+      }
+
+      setImageSrc(event.target?.result);
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   return (
     <section className='flex items-center justify-center m-6'>
-      <div className='relative bg-[#a5a6a9] w-[354px] h-[221px] text-[#252525] rounded-md cursor-pointer'>
-        <div className='absolute-center border-dashed border-2 border-indigo-600 w-[334px] h-[201px]' />
-        <div className='flex items-center justify-center w-full h-full'>
+      <input
+        ref={inputFileRef}
+        className='hidden'
+        type='file'
+        accept='image/*'
+        onChange={onChagne}
+      />
+      <button
+        className='relative bg-[#a5a6a9] w-[354px] h-[221px] hover:bg-[#352772]
+        hover:text-[white] text-[#252525] rounded-md cursor-pointer'
+        onClick={onClick}
+        type='button'
+      >
+        <span className='inline-flex items-center justify-center
+          border-dashed border-2 border-indigo-600 w-[334px] h-[201px]'
+        >
           이미지 파일을 올려놓으세요.
-        </div>
-      </div>
+        </span>
+      </button>
     </section>
   );
 }
--- src/store/sprite_size.ts
+++ src/store/sprite_size.ts
@@ -28,8 +28,8 @@ export const imageSizeState = atom<Size>({
 
 export const imageSrcState = atom<string>({
   key: REPEAT_COORD,
-  // default: 'file:///Users/zum/Desktop/sp_weather_time_b8ecd0.png',
-  default: 'https://s.pstatic.net/static/www/img/uit/sp_weather_time_b8ecd0.png',
+  default: '',
+  // default: 'https://s.pstatic.net/static/www/img/uit/sp_weather_time_b8ecd0.png',
   // default: 'https://cmnt.zum.com/plugin/zum-comment/images/spr_social_comment_m_20190902.png',
   // default: 'https://s.pstatic.net/static/www/img/uit/sp_main_947f65.png',
   // default: 'https://i.pinimg.com/564x/6a/ea/1c/6aea1c0bc96840a03644ed7b460fac9e.jpg',
