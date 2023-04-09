22739a591f16fa8bebcc00a618c50343ae0b2355
dkpark10 <dkpark10@naver.com>
2023-04-09 23:12:55 +0900

    [test]: cypress 테스트 초안 작성

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,9 +1,20 @@
 describe('이미지 스프라이트 테스트', () => {
   beforeEach(() => {
-    cy.exec('yarn dev');
+    // cy.exec('yarn dev');
   });
 
-  it('사이트 방문', () => {
-    cy.visit('/');
+  it('탭 전환시 이전 드래그 했던 정보들을 렌더링 한다.', () => {
+    // cy.visit('https://dkpark10.github.io/img_spriter/');
+    cy.visit('http://localhost:3000/');
+
+    const steps = ['1', '1.02', '1.04', '1.06', '1.08', '1.1', '1.12', '1.14', '1.16', '1.18', '1.2', '1.22', '1.24'];
+
+    cy.get('input[type=range]')
+      .invoke('val', 1.24)
+      .trigger('change', { data: 1.24 })
+      .click();
+
+    cy.get('[data-testid="scale_text"]')
+      .contains('이미지 사이즈 조절: 1.24');
   });
 });
--- src/components/size_scale_range.tsx
+++ src/components/size_scale_range.tsx
@@ -20,15 +20,18 @@ export default function ImageScaleRangeBar() {
   return (
     <div className='flex items-center justify-center mt-5'>
       <div>
-        <div className='text-center'>
-          이미지 사이즈 조절:
-          &nbsp;
+        <div
+          data-testid='scale_text'
+          className='text-center'
+        >
+          이미지 사이즈 조절:&nbsp;
           {imageState.scale}
         </div>
         <input
+          data-testid='scale_bar'
           className='w-96'
           type='range'
-          name='size_scale_bar'
+          name='scale_bar'
           min={0.5}
           max={1.5}
           step={0.02}
