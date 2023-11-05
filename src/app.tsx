130a6eb21efb68e6dc790ab2f1d3fa5528c28077
dkpark10 <dkpark10@naver.com>
2023-11-05 22:46:56 +0900

    v1.5.1

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ export default function App() {
   const imageState = useRecoilValue(currentImageState);
   return (
     <>
-      <span className="hidden" data-version={packageInfo.version || '1.5.0'} />
+      <span className="hidden" data-version={packageInfo.version || '1.5.1'} />
       <ToolBox />
       <Title />
       <Header />
