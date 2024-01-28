3b9d204b109f9b095c33dd05d74343167e58b17d
dkpark10 <dkpark10@naver.com>
2024-01-28 23:46:44 +0900

    chore: 툴박스 렌더링

--- src/components/tool-box.tsx
+++ src/components/tool-box.tsx
@@ -31,47 +31,39 @@ IconContainer.defaultProps = {
 
 export default function ToolBox() {
   const [colorRect, setColorRect] = useRecoilState(currentRectColor);
-  const portalRef = useRef<HTMLElement | null>(null);
-  const colorElementRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    portalRef.current = document.getElementById('portal');
-  }, []);
+  const colorElementRef = useRef<HTMLInputElement>(null);
 
   const onClickColorTool = () => {
     if (!colorElementRef.current) return;
     colorElementRef.current.click();
   };
 
-  if (portalRef.current) {
-    return createPortal(
-      <div className="absolute top-40 rounded left-11 border border-neutral-600">
-        <IconContainer onClick={onClickColorTool}>
-          <input
-            ref={colorElementRef}
-            className="w-0 h-0"
-            type="color"
-            name="color"
-            value={colorRect}
-            onChange={(e) => {
-              setColorRect(e.target.value as HexColor);
-            }}
-          />
-          <IoMdColorFill size={23} />
-        </IconContainer>
-        <IconContainer>
-          <MdAutoFixHigh size={23} />
-        </IconContainer>
-        <IconContainer>
-          <TbBoxAlignBottomRightFilled size={23} />
-        </IconContainer>
-        <IconContainer>
-          <TbBoxAlignBottomRight size={23} />
-        </IconContainer>
-      </div>,
-      portalRef.current,
-    );
-  }
-
-  return null;
+  return createPortal(
+    <div className="absolute top-40 rounded left-11 border border-neutral-600">
+      <IconContainer onClick={onClickColorTool}>
+        <input
+          ref={colorElementRef}
+          className="w-0 h-0"
+          type="color"
+          name="color"
+          value={colorRect}
+          onChange={(e) => {
+            setColorRect(e.target.value as HexColor);
+          }}
+        />
+        <IoMdColorFill size={23} />
+      </IconContainer>
+      <IconContainer>
+        <MdAutoFixHigh size={23} />
+      </IconContainer>
+      <IconContainer>
+        <TbBoxAlignBottomRightFilled size={23} />
+      </IconContainer>
+      <IconContainer>
+        <TbBoxAlignBottomRight size={23} />
+      </IconContainer>
+    </div>,
+    document.getElementById('portal') as Element,
+  );
 }
