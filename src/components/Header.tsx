2be6be1ce38e8422e0414bb3e6f1105d08fc14f7
dkpark10 <dkpark10@naver.com>
2024-01-28 23:42:30 +0900

    refactor: 리액트 탭스로 이주

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,42 @@
 import { TabName } from 'custom-type';
-import React from 'react';
 import { useRecoilState } from 'recoil';
 import InputSrc from './input_src';
 import DropBox from './drop_box';
 import { currentTabAtom } from '@/store';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 export default function Header() {
   const tabNames: TabName[] = ['이미지 경로 검색', '이미지 파일 업로드'];
 
   const [currentTab, setCurrentTab] = useRecoilState<TabName>(currentTabAtom);
 
-  const onClick = (tabName: TabName) => {
-    setCurrentTab(tabName);
-  };
+  const tabIndex = currentTab === '이미지 경로 검색' ? 0 : 1;
 
   return (
     <>
-      <header className="w-full flex justify-center items-center my-8 border-b-2 border-solid border-blue-600">
-        {tabNames.map((tabName, idx) => {
-          const currentTabClass = currentTab === tabName ? 'text-white bg-blue-600' : '';
+      <header>
+        <Tabs className="w-full flex justify-center items-center my-8 border-b-2 border-solid border-blue-600" selectedIndex={tabIndex} onSelect={(idx) => setCurrentTab(tabNames[idx])}>
+          <TabList className='flex justify-between'>
+            {tabNames.map((tabName, idx) => {
+              const currentTabClass = currentTab === tabName ? 'text-white bg-blue-600' : '';
 
-          return (
-            <div
-              className={`inline-flex justify-center items-center rounded-t-md  w-[170px] h-[34px] ${currentTabClass}`}
-              key={tabName}
-              role="button"
-              tabIndex={idx}
-              onClick={() => onClick(tabName)}
-              onKeyUp={() => onClick(tabName)}
-            >
-              {tabName}
-            </div>
-          );
-        })}
+              return (
+                <Tab key={tabName} className="">
+                  <div
+                    tabIndex={idx}
+                    className={`inline-flex justify-center items-center rounded-t-md w-[170px] h-[34px] ${currentTabClass} cursor-pointer	`}
+                  >
+                    {tabName}
+                  </div>
+                </Tab>
+              );
+            })}
+          </TabList>
+          {tabNames.map((tabName) => (
+            <TabPanel key={tabName} />
+          ))}
+        </Tabs>
       </header>
       {currentTab === '이미지 경로 검색' ? <InputSrc /> : <DropBox />}
     </>
