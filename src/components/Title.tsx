1a3850f37c10be0b7a4fbf8cdc557259c3abf0dc
dkpark10 <dkpark10@naver.com>
2024-04-13 01:40:05 +0900

    refactor: 타이틀 h1 태그 변경

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Title(): JSX.Element {
-  return <div className="flex justify-center items-center text-[#19191c] text-4xl m-3.5">이미지 스프라이터</div>;
+  return <h1 className="flex justify-center items-center text-[#19191c] text-4xl m-3.5">이미지 스프라이터</h1>;
 }
