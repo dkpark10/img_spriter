d7858444b6d8a1bf29be9d37e9e0f72e9447ea29
dkpark10 <dkpark10@naver.com>
2024-05-15 19:38:01 +0900

    chore: playwright 버전업

--- tests/canvas.test.ts
+++ tests/canvas.test.ts
@@ -4,46 +4,48 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000');
 });
 
-test('탭 전환 후에도 이전 렌더링 정보가 남아야 한다.', async ({ page }) => {
-  const boundingBox = await page.locator('canvas').boundingBox();
-  if (!boundingBox) throw new Error('empty src canvas');
+test.describe('캔버스 테스트', () => {
+  test('탭 전환 후에도 이전 렌더링 정보가 남아야 한다.', async ({ page }) => {
+    const boundingBox = await page.locator('canvas').boundingBox();
+    if (!boundingBox) throw new Error('empty src canvas');
 
-  await page.mouse.move(boundingBox.x, boundingBox.y);
-  await page.mouse.down();
+    await page.mouse.move(boundingBox.x, boundingBox.y);
+    await page.mouse.down();
 
-  for (let i = 0; i <= 100; i += 10) {
-    await page.mouse.move(boundingBox.x + i, boundingBox.y + i);
-  }
+    for (let i = 0; i <= 100; i += 10) {
+      await page.mouse.move(boundingBox.x + i, boundingBox.y + i);
+    }
 
-  await page.mouse.up();
-  await page.screenshot({ path: './tests/canvas-src.png' });
+    await page.mouse.up();
+    await page.screenshot({ path: './tests/canvas-src.png' });
 
-  expect(await page.getByTestId('code-width').textContent()).toBe('100px');
-  expect(await page.getByTestId('code-height').textContent()).toBe('100px');
+    expect(await page.getByTestId('code-width').textContent()).toBe('100px');
+    expect(await page.getByTestId('code-height').textContent()).toBe('100px');
 
-  await page.getByRole('tab', { name: /이미지 파일 업로드/i }).click();
+    await page.getByRole('tab', { name: /이미지 파일 업로드/i }).click();
 
-  await page.getByTestId('file_button').click();
-  await page.locator('input[type=file]').setInputFiles('./tests/sample2.png');
+    await page.getByTestId('file_button').click();
+    await page.locator('input[type=file]').setInputFiles('./tests/sample2.png');
 
-  await page.screenshot({ path: './tests/canvas-file.png' });
+    await page.screenshot({ path: './tests/canvas-file.png' });
 
-  const boundingBox2 = await page.locator('canvas').boundingBox();
-  if (!boundingBox2) throw new Error('empty file canvas');
+    const boundingBox2 = await page.locator('canvas').boundingBox();
+    if (!boundingBox2) throw new Error('empty file canvas');
 
-  await page.mouse.move(boundingBox2.x, boundingBox2.y);
-  await page.mouse.down();
+    await page.mouse.move(boundingBox2.x, boundingBox2.y);
+    await page.mouse.down();
 
-  /** @fix 60까지 밖에 드래그가 안됨 */
-  for (let i = 0; i <= 60; i += 10) {
-    await page.mouse.move(boundingBox2.x + i, boundingBox2.y + i);
-  }
-  
-  await page.mouse.up();  
-  expect(await page.getByTestId('code-width').textContent()).toBe('60px');
-  expect(await page.getByTestId('code-height').textContent()).toBe('60px');
+    /** @fix 60까지 밖에 드래그가 안됨 */
+    for (let i = 0; i <= 100; i += 10) {
+      await page.mouse.move(boundingBox2.x + i, boundingBox2.y + i);
+    }
 
-  await page.getByRole('tab', { name: /이미지 경로 검색/i }).click();
-  expect(await page.getByTestId('code-width').textContent()).toBe('100px');
-  expect(await page.getByTestId('code-height').textContent()).toBe('100px');
+    await page.mouse.up();
+    expect(await page.getByTestId('code-width').textContent()).toBe('60px');
+    expect(await page.getByTestId('code-height').textContent()).toBe('60px');
+
+    await page.getByRole('tab', { name: /이미지 경로 검색/i }).click();
+    expect(await page.getByTestId('code-width').textContent()).toBe('100px');
+    expect(await page.getByTestId('code-height').textContent()).toBe('100px');
+  });
 });
