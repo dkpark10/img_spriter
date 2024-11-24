dfb5b46669c08ae5337de6242030e404c711db6e
dkpark10 <dkpark10@naver.com>
2024-11-24 20:01:40 +0900

    style: 인풋 컬러 레드 삭제

--- src/components/input_src.tsx
+++ src/components/input_src.tsx
@@ -1,7 +1,6 @@
 import type { ImageState } from 'custom-type';
 import { useMemo } from 'react';
 import { useRecoilState } from 'recoil';
-import clsx from 'clsx';
 import { pathImageAtom } from '@/store';
 import { debounce } from '@/utils';
 import { useInput } from '@/hooks';
@@ -28,7 +27,7 @@ export default function Header(): JSX.Element {
   return (
     <div className="w-full flex justify-center items-center h-[60px]">
       <input
-        className={`w-[408px] border-none h-[38px] rounded-md bg-[#e0e0e0] px-2 ${clsx(!imageState.loadSuccess && 'text-rose-600')}`}
+        className="w-[408px] border-none h-[38px] rounded-md bg-[#e0e0e0] px-2"
         type="text"
         name="imgsrc"
         placeholder="이미지 주소를 입력하세요."
